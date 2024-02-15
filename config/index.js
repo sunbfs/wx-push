c313675fd1d9a8b3590b3568cf2248ef3879de32
sunbfs <57968087+sunbfs@users.noreply.github.com>
2024-02-15 18:37:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx2d5d3c63db5ecba9",
   appsecret: "1759509e5656b1bb9128c6ddf7db41e5",
-  template_id: "0qpIvJryydYorVzUXTsteMjz0HORZd_h31bc_hsG9KA",
+  template_id: "A1JGPu0dh1ltBk_Xc4xUUJkrOi-zm08I2Nn0sZ1jBGY",
   touser: "oPBB9jndc9yYx-cBP60xVB0PZWOc",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
