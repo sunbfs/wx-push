892988a7a32ebc814383ba36b3c161d188d2c06a
sunbfs <57968087+sunbfs@users.noreply.github.com>
2024-02-15 16:31:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx2884e4c91266b27f",
+  appsecret: "351f797358cc67e68afa574b1aef7bc3",
+  template_id: "wBLsNlKwEH_XUbsUR3GkeHaXi6QwqI28lPYeY40wnRU",
+  touser: "ovhYX6VfQJxolWdR5thrrvDBre8M",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "西安",
+  first_date: "2024-02-15",
 };
